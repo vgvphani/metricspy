ad7219090fab25b0e343a930b3f5882dc58d748d
sandeepchikkala <cssandeep.rsh@gmail.com>
2017-11-07 11:18:36 +0100

    updated formatiing
    Ordered output.

--- metricspyjason.py
+++ metricspyjason.py
@@ -1,12 +1,14 @@
-import socket
+import collections
 import psutil
+import socket
 import time
 import json
 while True :
- host = (socket.gethostname())
+ hostname = socket.gethostname()
  mem = psutil.virtual_memory()
  cpu = psutil.cpu_percent(interval=3)
  disk = psutil.disk_usage('/')
- allstats = {'memU' : mem[1], 'diskU' : disk[3], 'cpuU' : cpu, 'host': host }
- print json.dumps(allstats)
+ totusg = [('Host', hostname), ('memU', mem[1]),('diskU', disk[3]), ('cpuU', cpu)]
+ totusg = collections.OrderedDict(totusg) 
+ print  json.dumps(totusg)
  time.sleep(3)
